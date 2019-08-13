6d1fd57be204b5b0faa881153288200fa614cb64
gaoguanqi <307590625@qq.com>
2019-08-13 16:28:27 +0800

    appbar

--- flutter_app/lib/utils/utils.dart
+++ flutter_app/lib/utils/utils.dart
@@ -8,7 +8,7 @@ class Utils {
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.CENTER,
         timeInSecForIos: 1,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey,
         textColor: Colors.white,
         fontSize: 14.0);
   }
